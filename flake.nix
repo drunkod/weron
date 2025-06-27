6b5402ed6aac922379b6402b8fa3529e3ddf2dad
drunkod <drunkod@gmail.com>
2025-06-27 08:03:24 +0000

    add flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
-  description = "A Nix flake for building and running the weron CLI";
+  description = "A Nix flake for building and running the weron CLI and its tutorials";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    # Use 'self' to refer to the current flake's directory as the source.
-    # This is simpler than fetching from GitHub again.
   };
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -15,14 +13,8 @@
 
         weronPackage = pkgs.buildGoModule {
           pname = "weron";
-          # The version can be set dynamically or hard-coded.
           version = "0.3.0";
-
-          # 'src' points to the root of your project directory.
           src = self;
-
-          # --- THIS IS THE KEY CORRECTION ---
-          # We point the build to the correct Go package containing main.go
           subPackages = [ "cmd/weron" ];
 
           # Nix needs a hash of the Go dependencies for a reproducible build.
@@ -32,43 +24,164 @@
           # Statically link the binary for better portability.
           ldflags = [ "-s" "-w" ];
         };
+
+        # Helper to create a simple app that runs a weron subcommand
+        mkWeronApp = name: extraFlags: pkgs.writeShellScriptBin name ''
+          #!${pkgs.stdenv.shell}
+          echo "Running: weron ${extraFlags} $@"
+          exec ${weronPackage}/bin/weron ${extraFlags} "$@"
+        '';
+
       in
       {
-        # 'packages' define what can be built or installed.
         packages = {
           default = weronPackage;
           weron = weronPackage;
         };
 
-        # 'apps' allow running packages directly with 'nix run'.
         apps = {
           default = {
             type = "app";
-            program = "${self.packages.${system}.default}/bin/weron";
+            program = "${weronPackage}/bin/weron";
+          };
+
+          signaler = {
+            type = "app";
+            program = "${pkgs.writeShellScriptBin "run-signaler" ''
+              #!${pkgs.stdenv.shell}
+              echo "### Starting a local weron signaler ###"
+              echo "This starts a simple, in-memory signaling server for testing."
+              echo "It will be available at ws://localhost:1337"
+              echo ""
+              echo "For production, you should set up an external PostgreSQL and Redis"
+              echo "and run the signaler with --postgres-url and --redis-url flags."
+              echo "You must also provide an API password for the manager."
+              echo ""
+              echo "Example for testing with a password:"
+              echo "  nix run .#signaler -- --api-password 'myapipassword'"
+              echo "--------------------------------------------------------"
+              exec ${weronPackage}/bin/weron signaler "$@"
+            ''}/bin/run-signaler";
+          };
+
+          manager-list = {
+            type = "app";
+            program = "${pkgs.writeShellScriptBin "run-manager-list" ''
+              #!${pkgs.stdenv.shell}
+              echo "### Listing communities ###"
+              echo "Note: This requires a running signaler. Set env vars if needed:"
+              echo "  export WERON_RADDR='http://localhost:1337/'"
+              echo "  export API_PASSWORD='myapipassword'"
+              echo "--------------------------------------------------------"
+              exec ${weronPackage}/bin/weron manager list "$@"
+            ''}/bin/run-manager-list";
+          };
+
+          # --- CORRECTED SECTION ---
+          # All apps now have the correct { type = "app"; program = "..."; } structure.
+          manager-create = {
+            type = "app";
+            program = "${mkWeronApp "manager-create" "manager create"}/bin/manager-create";
+          };
+          manager-delete = {
+            type = "app";
+            program = "${mkWeronApp "manager-delete" "manager delete"}/bin/manager-delete";
+          };
+          # --- END CORRECTED SECTION ---
+
+          chat = {
+            type = "app";
+            program = "${pkgs.writeShellScriptBin "run-chat" ''
+              #!${pkgs.stdenv.shell}
+              echo "### Starting weron chat ###"
+              echo "Example to connect to the public signaler:"
+              echo '  nix run .#chat -- --community mycommunity --password mypassword --key mykey --names user1'
+              echo ""
+              echo "To connect to a local signaler, first run 'nix run .#signaler' in another terminal, then:"
+              echo '  nix run .#chat -- --raddr ws://localhost:1337 --community mycommunity --password mypassword --key mykey --names user1'
+              echo "--------------------------------------------------------"
+              exec ${weronPackage}/bin/weron chat "$@"
+            ''}/bin/run-chat";
+          };
+
+          # --- CORRECTED SECTION ---
+          latency-server = {
+            type = "app";
+            program = "${mkWeronApp "latency-server" "utility latency --server"}/bin/latency-server";
+          };
+          latency-client = {
+            type = "app";
+            program = "${mkWeronApp "latency-client" "utility latency"}/bin/latency-client";
+          };
+          throughput-server = {
+            type = "app";
+            program = "${mkWeronApp "throughput-server" "utility throughput --server"}/bin/throughput-server";
+          };
+          throughput-client = {
+            type = "app";
+            program = "${mkWeronApp "throughput-client" "utility throughput"}/bin/throughput-client";
+          };
+          # --- END CORRECTED SECTION ---
+
+          vpn-ip = {
+            type = "app";
+            program = "${pkgs.writeShellScriptBin "run-vpn-ip" ''
+              #!${pkgs.stdenv.shell}
+              echo "### Starting weron IP VPN (Layer 3) ###"
+              if [ "$(id -u)" -ne 0 ]; then
+                  echo ""
+                  echo "!!!!!!!!!!!!!!!!!! PERMISSION ERROR !!!!!!!!!!!!!!!!!! "
+                  echo "This command requires network administrator privileges to create a TUN device."
+                  echo "The app will now exit. To run it correctly, please use 'sudo':"
+                  echo ""
+                  echo "  sudo nix run .#vpn-ip -- [YOUR-VPN-FLAGS]"
+                  echo ""
+                  echo "Example:"
+                  echo "  sudo nix run .#vpn-ip -- --community mycommunity --password mypassword --key mykey --ips 10.0.0.1/24"
+                  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                  exit 1
+              fi
+              exec ${weronPackage}/bin/weron vpn ip "$@"
+            ''}/bin/run-vpn-ip";
+          };
+          vpn-ethernet = {
+            type = "app";
+            program = "${pkgs.writeShellScriptBin "run-vpn-ethernet" ''
+              #!${pkgs.stdenv.shell}
+              echo "### Starting weron Ethernet VPN (Layer 2) ###"
+              if [ "$(id -u)" -ne 0 ]; then
+                  echo ""
+                  echo "!!!!!!!!!!!!!!!!!! PERMISSION ERROR !!!!!!!!!!!!!!!!!! "
+                  echo "This command requires network administrator privileges to create a TAP device."
+                  echo "The app will now exit. To run it correctly, please use 'sudo':"
+                  echo ""
+                  echo "  sudo nix run .#vpn-ethernet -- [YOUR-VPN-FLAGS]"
+                  echo ""
+                  echo "Example:"
+                  echo "  sudo nix run .#vpn-ethernet -- --community mycommunity --password mypassword --key mykey"
+                  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                  exit 1
+              fi
+              exec ${weronPackage}/bin/weron vpn ethernet "$@"
+            ''}/bin/run-vpn-ethernet";
           };
         };
 
-        # 'devShells' define development environments.
         devShells.default = pkgs.mkShell {
           name = "weron-dev-shell";
-
-          # Tools available in the development shell.
-          buildInputs = [
-            weronPackage # The 'weron' command itself.
-            pkgs.go      # The Go compiler and tools.
-          ];
-
+          buildInputs = [ weronPackage pkgs.go ];
           shellHook = ''
             echo "### weron Development Shell ###"
-            echo "The 'weron' executable (built from your local source) is in your PATH."
-            echo "The Go toolchain is also available."
+            echo "The 'weron' executable (from your local source) is in your PATH."
             echo ""
-            echo "You can now run weron commands, for example:"
-            echo "  weron --help"
+            echo "--- IMPORTANT: For VPN Usage ---"
+            echo "To use 'weron vpn ...', the binary needs network admin capabilities."
+            echo "If you are not in a pre-configured container, grant them by running:"
+            echo "  sudo setcap cap_net_admin+ep $(which weron)"
+            echo "--------------------------------"
           '';
         };
 
-        # Standard formatter for Nix code. Run with 'nix fmt'.
         formatter = pkgs.nixpkgs-fmt;
       }
     );
